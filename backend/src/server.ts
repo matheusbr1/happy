2bd6e8d6345902534f30255e245dfee2bb19293f
matheusbr1 <matheusbaron10@gmail.com>
2020-10-13 21:35:06 -0300

    [Workshop 02] Database created

---
+++ backend/src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+
+import './database/connection'
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/users', (request, response) => {
+    response.json({ message: 'Hello world' })
+})
+
+app.listen(3333)
